d35861b01674e2e3decf57d397bc985f6e2c11dc
Paco <you@example.com>
2020-02-10 12:12:12 +0100

    maincontroller

--- src/main/java/com/alfonsotienda/holaspring/controller/MainController.java
+++ src/main/java/com/alfonsotienda/holaspring/controller/MainController.java
@@ -1,8 +1,11 @@
+
 package com.alfonsotienda.holaspring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * MainController
@@ -13,8 +16,10 @@ public class MainController {
 
     @GetMapping("/")
     @ResponseBody
-    public String holaMundo(){
-        return "Hola Mundo";
+    public String holaMundo(@RequestParam ("nombre") String name,
+                            @RequestParam ("edad") Integer edad  ){
+
+        return "Hola "+name + " tienes " + edad + " años " ;
     }
 
     @GetMapping("/ingles")
@@ -22,5 +27,15 @@ public class MainController {
     public String helloWorld(){
         return "Hello World";
     }
-    
+   
+    @GetMapping("/nuevo")
+    public ModelAndView helloHTML() {
+       ModelAndView respuesta = new ModelAndView ("hello");
+       String msg = "Hola mundo html";
+       respuesta.addObject("mensaje", msg);
+       return respuesta;
+
+   } 
+
+
 }
