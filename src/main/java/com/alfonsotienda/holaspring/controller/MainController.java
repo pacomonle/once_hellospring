e3e71798fb78422f4d4fcaf5f8a5eec1fb48f9e0
Paco <you@example.com>
2020-02-10 13:45:32 +0100

    calculadora2

--- src/main/java/com/alfonsotienda/holaspring/controller/MainController.java
+++ src/main/java/com/alfonsotienda/holaspring/controller/MainController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import java.util.Scanner; // Importación de la clase Scanner.
 
-
-
 /**
  * MainController
  */
@@ -19,73 +17,60 @@ public class MainController {
 
     @GetMapping("/")
     @ResponseBody
-    public String holaMundo(@RequestParam ("nombre") String name,
-                            @RequestParam ("edad") Integer edad  ){
+    public String holaMundo(@RequestParam("nombre") String name, @RequestParam("edad") Integer edad) {
 
-        return "Hola "+name + " tienes " + edad + " años " ;
+        return "Hola " + name + " tienes " + edad + " años ";
     }
 
     @GetMapping("/ingles")
     @ResponseBody
-    public String helloWorld(){
+    public String helloWorld() {
         return "Hello World";
     }
-   
+
     @GetMapping("/nuevo")
     public ModelAndView helloHTML() {
-       ModelAndView respuesta = new ModelAndView ("hello");
-       String msg = "Hola mundo html";
-       respuesta.addObject("mensaje", msg);
-       return respuesta;
+        ModelAndView respuesta = new ModelAndView("hello");
+        String msg = "Hola mundo html";
+        respuesta.addObject("mensaje", msg);
+        return respuesta;
 
-   } 
-
-   @GetMapping("/calculadora")
-   @ResponseBody
-
-   public String calculadora(@RequestParam ("numero1") Double num1,
-                            @RequestParam ("numero2") Double num2,
-                            @RequestParam ("operador") String operacion){
-
-       double res = 0;
-       boolean comprobar = false;
-
-       do {  
-       if (operacion.equals("+") || operacion.equals("-") || operacion.equals("x") ||
-           operacion.equals("X") || operacion.equals("/") || operacion.equals("*")) {
-           comprobar = true;
-       }else { comprobar = false; }
-       } while (comprobar != true); 
-         
-       do{
-        comprobar = true;
-        switch(operacion){
-            case "+":
-                res = num1 + num2;
-                break;
-            case "-":
-                res = num1 - num2;
-                break;
-            case "x":
-            case "X":
-            case "*":
-                res = num1 * num2;
-                break;
-            case "/":
-                res = num1 / num2;
-                break;
-            
-            }
-        }while(comprobar != true);  
-
-        
-                                return "el resultado de " + num1 + " " + operacion + " " + num2 + " es " + res ;
     }
 
-   
+    @GetMapping("/calculadora")
+    @ResponseBody
 
+    public String calculadora(@RequestParam("numero1") Double num1, @RequestParam("numero2") Double num2,
+            @RequestParam("operador") String operacion) {
 
+        double res = 0;
 
+        if (operacion.equals("+") || operacion.equals("-") || operacion.equals("x") || operacion.equals("X")
+                || operacion.equals("/") || operacion.equals("*")) {
 
+        } else {
+            return "que noooooooo";
+        }
+
+        switch (operacion) {
+        case "+":
+            res = num1 + num2;
+            break;
+        case "-":
+            res = num1 - num2;
+            break;
+        case "x":
+        case "X":
+        case "*":
+            res = num1 * num2;
+            break;
+        case "/":
+            res = num1 / num2;
+            break;
+
+        }
+
+        return "el resultado de " + num1 + " " + operacion + " " + num2 + " es " + res;
+    }
 
 }
