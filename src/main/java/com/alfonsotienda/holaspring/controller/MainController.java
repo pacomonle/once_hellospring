f884c853ffee41dc1f15168322bd5f46f06798a2
pacomonle <pacomonle@live.com>
2020-02-11 20:14:04 +0100

    TEST PRUEBA 1

--- src/main/java/com/alfonsotienda/holaspring/controller/MainController.java
+++ src/main/java/com/alfonsotienda/holaspring/controller/MainController.java
@@ -40,46 +40,104 @@ public class MainController {
     @GetMapping("/test")
     @ResponseBody
     public String testdelocura(@RequestParam("opcion1") String opcion1, @RequestParam("opcion2") String opcion2,
-            @RequestParam("opcion3") String opcion3, @RequestParam("opcion4") String respuesta4) {
-         String test1, test2, test3, test4, test5, test6, test7, test8; 
-         int suma1, suma2, suma3, suma4, suma5, suma6, suma7, suma8 =0;  
+            @RequestParam("opcion3") String opcion3) {
+         String test1, test2, test3, test4, test5; 
+         int suma1, suma2, suma3, suma4, suma5 =0;  
          String respuesta1= "nunca";
-         String respuesta2= "una vez al año";
-         String respuesta3= "a menudo ";
-         String repuesta4= "son las tres menos cuarto";  
+         String respuesta2= "de vez encuando ";
+         String repuesta3= "son las tres menos cuarto";  
        
-         System.out.println("respuesta1= nunca " + "respuesta2= una vez al año " +
-                            "respuesta3= a menudo " + "respuesta4= son las tres menos cuarto " );
-         
 
-        System.out.println("¿con que frecuencia te dicen estas loco?"); 
-        
+        System.out.println("BIENVENIDO AL TEST 2.0 ULTIMA GENRACION - SUPER FIVE QUESTIONS ¿ ESTOY CUERDO ?");
+
+       /*
+        System.out.println(" respuesta1= nunca " + " respuesta2= de vez encuando " +
+                            " respuesta3= son las tres menos cuarto " );
+         
+         */
+         
+        System.out.println("1.¿con que frecuencia te dicen estas loco?"); 
+            test1 = System.console().readLine();
            if (test1.equals(respuesta1)){
                suma1 = 0;
            }else if (test1.equals(respuesta2)){
                suma1 = 1;
            }else if (test1.equals(respuesta3)){
                suma1 = 2;
-           }else if (test1.equals(respuesta4)){
-               suma1 = 3;
            }
          
-           
+        System.out.println("2.¿cuantas veces has acabado desnudo en una fiesta?"); 
+            test2 = System.console().readLine();
+           if (test2.equals(respuesta1)){
+               suma2 = 0;
+           }else if (test2.equals(respuesta2)){
+               suma2 = 1;
+           }else if (test2.equals(respuesta3)){
+               suma2 = 2;
+           }
          
+        System.out.println("3.¿cuantas veces le has quitado el sitio a una ancianita en el autobus?"); 
+            test3 = System.console().readLine();
+           if (test3.equals(respuesta1)){
+               suma3 = 0;
+           }else if (test3.equals(respuesta2)){
+               suma3 = 1;
+           }else if (test3.equals(respuesta3)){
+               suma3 = 2;
+           }
 
-                  
-             
+        System.out.println("4.¿cuando un compañero te ha pedido ayuda le has contestado jurando y maldiciendo en arameo (valido otro idioma)?"); 
+            test4 = System.console().readLine();
+           if (test4.equals(respuesta1)){
+               suma4 = 0;
+           }else if (test4.equals(respuesta2)){
+               suma4 = 1;
+           }else if (test3.equals(respuesta3)){
+               suma4 = 2;
+           }
+
+        System.out.println("5.¿cuandote dicen que hay examen del curso de programacion , te pones mas nervioso que espinete vendiendo condones?"); 
+            test5 = System.console().readLine();
+           if (test5.equals(respuesta1)){
+               suma5 = 0;
+           }else if (test4.equals(respuesta2)){
+               suma5 = 1;
+           }else if (test3.equals(respuesta3)){
+               suma5 = 2;
+           }
+
+    int suma;
+    suma = suma1 + suma2 + suma3 + suma4 + suma5;
+    System.out.println("\nHas obtenido " + suma + " puntos.");
+    
+    System.out.println();
+
+       if (suma>=o and suma<4){
+           return "estas al principio de la escala, completamente cuerdo pero MUY ABURRIDO";
+       }else if (suma>=4 and suma<9){
+           return "estas en la parte media de la escala, vas en CAMINO HACIA LA LOCURA, BIEN!!!";
+       }else if (suma=9 or suma=10){
+           return "enhorabuena ERES DIOS, eres el nuevo NORMAN BATES!!!";
+           }
+
+ }
 
 
-            }
+private int calcula1(
+    String opcion1,
+    String opcion2,
+    String opcion3
+) {
+    int suma1 = 0;
 
-
-
-
-
-
-
-
+           if (opcion1.equals(respuesta1)){
+              return suma1 = 0;
+           }else if (opcion2.equals(respuesta2)){
+              return suma1 = 1;
+           }else if (opcion3.equals(respuesta3)){
+             return suma1 = 2;
+           }
+  }
 
 
 
