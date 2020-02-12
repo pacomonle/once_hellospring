53635a6b64ac9bf01e46515ca99885ac6a23fd9b
pacomonle <pacomonle@live.com>
2020-02-12 07:34:14 +0100

    test prueba 2

--- src/main/java/com/alfonsotienda/holaspring/controller/MainController.java
+++ src/main/java/com/alfonsotienda/holaspring/controller/MainController.java
@@ -39,105 +39,104 @@ public class MainController {
    
     @GetMapping("/test")
     @ResponseBody
-    public String testdelocura(@RequestParam("opcion1") String opcion1, @RequestParam("opcion2") String opcion2,
-            @RequestParam("opcion3") String opcion3) {
-         String test1, test2, test3, test4, test5; 
-         int suma1, suma2, suma3, suma4, suma5 =0;  
-         String respuesta1= "nunca";
-         String respuesta2= "de vez encuando ";
-         String repuesta3= "son las tres menos cuarto";  
-       
-
-        System.out.println("BIENVENIDO AL TEST 2.0 ULTIMA GENRACION - SUPER FIVE QUESTIONS ¿ ESTOY CUERDO ?");
-
-       /*
-        System.out.println(" respuesta1= nunca " + " respuesta2= de vez encuando " +
-                            " respuesta3= son las tres menos cuarto " );
-         
-         */
-         
-        System.out.println("1.¿con que frecuencia te dicen estas loco?"); 
-            test1 = System.console().readLine();
-           if (test1.equals(respuesta1)){
-               suma1 = 0;
-           }else if (test1.equals(respuesta2)){
-               suma1 = 1;
-           }else if (test1.equals(respuesta3)){
-               suma1 = 2;
-           }
-         
-        System.out.println("2.¿cuantas veces has acabado desnudo en una fiesta?"); 
-            test2 = System.console().readLine();
-           if (test2.equals(respuesta1)){
-               suma2 = 0;
-           }else if (test2.equals(respuesta2)){
-               suma2 = 1;
-           }else if (test2.equals(respuesta3)){
-               suma2 = 2;
-           }
-         
-        System.out.println("3.¿cuantas veces le has quitado el sitio a una ancianita en el autobus?"); 
-            test3 = System.console().readLine();
-           if (test3.equals(respuesta1)){
-               suma3 = 0;
-           }else if (test3.equals(respuesta2)){
-               suma3 = 1;
-           }else if (test3.equals(respuesta3)){
-               suma3 = 2;
-           }
-
-        System.out.println("4.¿cuando un compañero te ha pedido ayuda le has contestado jurando y maldiciendo en arameo (valido otro idioma)?"); 
-            test4 = System.console().readLine();
-           if (test4.equals(respuesta1)){
-               suma4 = 0;
-           }else if (test4.equals(respuesta2)){
-               suma4 = 1;
-           }else if (test3.equals(respuesta3)){
-               suma4 = 2;
-           }
-
-        System.out.println("5.¿cuandote dicen que hay examen del curso de programacion , te pones mas nervioso que espinete vendiendo condones?"); 
-            test5 = System.console().readLine();
-           if (test5.equals(respuesta1)){
-               suma5 = 0;
-           }else if (test4.equals(respuesta2)){
-               suma5 = 1;
-           }else if (test3.equals(respuesta3)){
-               suma5 = 2;
-           }
-
-    int suma;
-    suma = suma1 + suma2 + suma3 + suma4 + suma5;
-    System.out.println("\nHas obtenido " + suma + " puntos.");
+    public ModelAndView testlocuraHTML() {
+        ModelAndView modelAndView = new ModelAndView("test"); 
+        modelAndView.addObject("mensaje", "");
+        return modelAndView;
     
-    System.out.println();
+    }
 
-       if (suma>=o and suma<4){
-           return "estas al principio de la escala, completamente cuerdo pero MUY ABURRIDO";
-       }else if (suma>=4 and suma<9){
-           return "estas en la parte media de la escala, vas en CAMINO HACIA LA LOCURA, BIEN!!!";
-       }else if (suma=9 or suma=10){
-           return "enhorabuena ERES DIOS, eres el nuevo NORMAN BATES!!!";
+    @PostMapping("/test") 
+    @ResponseBody
+
+    public ModelAndView testloculraHTMLPost(@RequestParam("respuesta1") String respuesta1, @RequestParam("respuesta2") String respuesta2,
+            @RequestParam("respuesta3") String respuesta3, @RequestParam("respuesta4") String respuesta4,
+            @RequestParam("respuesta5") String respuesta5) {
+      
+      ModelAndView modelAndView = new ModelAndView("test");
+
+      String resultado = calcula1(respuesta1, respuesta2, respuesta3,
+                                respuesta4, respuesta5);
+
+      modelAndView.addObject("mensaje", resultado);
+                 
+       return modelAndView;
+
+    }
+    
+    
+private String calcula1(
+    String respuesta1, respuesta2, respuesta3, respuesta4, respuesta5;
+    ) {
+    int suma1, suma2, suma3, suma4, suma5 = 0;
+// pregunta 1
+           if (respuesta1.equals("0")){
+             suma1 = 0;
+           }else if (respuesta1.equals("1")){
+              suma1 = 1;
+           }else if (respuesta1.equals("2")){
+             suma1 = 2;
            }
 
- }
+  // pregunta 2         
+           if (respuesta2.equals("0")){
+            suma2 = 0;
+          }else if (respuesta2.equals("1")){
+             suma2 = 1;
+          }else if (respuesta2.equals("2")){
+            suma2 = 2;
+          } 
+  // pregunta 3        
+         if (respuesta3.equals("0")){
+           suma3 = 0;
+         }else if (respuesta3.equals("1")){
+           suma3 = 1;
+         }else if (respuesta3.equals("2")){
+           suma3 = 2;
+         } 
+  // pregunta 4       
+         if (respuesta4.equals("0")){
+            suma4 = 0;
+         }else if (respuesta4.equals("1")){
+            suma4 = 1;
+         }else if (respuesta4.equals("2")){
+            suma4 = 2;
+         } 
+   // pregunta 5       
+         if (respuesta5.equals("0")){
+            suma5 = 0;
+         }else if (respuesta5.equals("1")){
+            suma5 = 1;
+         }else if (respuesta5.equals("2")){
+            suma5 = 2;
+         } 
+
+ // calculo de la suma de puntos obtenidos en el test        
+
+         int suma;
+         suma = suma1 + suma2 + suma3 + suma4 + suma5;
+         
+         System.out.println("RESULTADOS TEST 2.0 ULTIMA GENRACION - SUPER FIVE QUESTIONS?");
+
+         System.out.println();
+
+         System.out.println("\nHas obtenido " + suma + " puntos.");
+         
+         System.out.println();
+     
+ // cerramos la funcion con el return de los resulatdos obtenidos segun nuestra clasificacion
+         if (suma>=o and suma<4){
+            return "estas al principio de la escala, completamente cuerdo pero MUY ABURRIDO";
+        }else if (suma>=4 and suma<9){
+            return "estas en la parte media de la escala, vas en CAMINO HACIA LA LOCURA, BIEN!!!";
+        }else if (suma=9 or suma=10){
+            return "enhorabuena ERES DIOS, eres el nuevo NORMAN BATES!!!";
+            }
+
+    } 
 
 
-private int calcula1(
-    String opcion1,
-    String opcion2,
-    String opcion3
-) {
-    int suma1 = 0;
-
-           if (opcion1.equals(respuesta1)){
-              return suma1 = 0;
-           }else if (opcion2.equals(respuesta2)){
-              return suma1 = 1;
-           }else if (opcion3.equals(respuesta3)){
-             return suma1 = 2;
-           }
-  }
+  
 
 
 
@@ -243,4 +242,7 @@ private String calcula(
 
     return "el resultado de " + numero1 + " " + operacion + " " + numero2 + " es " + res;
 }
+
+
+
 }
